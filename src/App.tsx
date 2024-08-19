5e1aef0acf468b54f32325220c82400656042af0
Kavin CS <146223781+Kavin-CS@users.noreply.github.com>
2024-08-19 23:31:22 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,7 @@ const App: React.FC = () => {
 
 
 
-  
+
 
 
   useEffect(() => {
@@ -63,7 +63,7 @@ const App: React.FC = () => {
     message: '',
   });
   const [loading, setLoading] = useState(false);
- // Adjust this regex based on your expected phone number format
+  // Adjust this regex based on your expected phone number format
   const [isFormValid, setIsFormValid] = useState(false);
 
   useEffect(() => {
@@ -95,22 +95,27 @@ const App: React.FC = () => {
     if (formRef.current) {
       emailjs.sendForm(
         'service_4yg12dp',           // Your service ID
-        'template_436e9dr',          // Your template ID
+        'template_436e9dqr',          // Your template ID
         formRef.current,             // The HTML form element, guaranteed to be non-null
         'K-8uwDqXUVmsdV_m9'          // Your public key
       )
         .then((result) => {
           setStatus('Email sent successfully!');
           setLoading(false);
+          if (formRef.current) {
+            formRef.current.reset();
+          }
         })
         .catch((error) => {
           setStatus('Failed to send email.');
           setLoading(false);
+          if (formRef.current) {
+            formRef.current.reset();
+          }
         });
     } else {
       setStatus('Form reference is null.');
       setLoading(false);
-
     }
   };
 
@@ -551,7 +556,7 @@ const App: React.FC = () => {
 
             </div>
             <div className='flex justify-end font-bold md:hidden '>
-              scroll &nbsp; &nbsp; <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+              scroll &nbsp; &nbsp; <svg className="w-6 h-6 text-black " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
               </svg>
 
@@ -622,24 +627,26 @@ const App: React.FC = () => {
                 placeholder="Leave a message..." required onChange={handleChange} value={formData.message} ></textarea>
             </div>
             <div className='mt-4 flex justify-center items-center'>
-              
+
 
               <button
                 disabled={!isFormValid}
                 type="submit"
-                className={`animate-pulse w-1/4 text-white font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:ring-4 focus:outline-none
+                className={`animate-pulse gap-4 flex justify-center items-center w-1/4 text-white font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:ring-4 focus:outline-none
       ${isFormValid ? 'bg-blue-700 hover:bg-blue-800 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
                     : 'bg-gray-400 cursor-not-allowed dark:bg-gray-500'}`}
               >
-               {
-                loading && <div role="status">
-                  <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
-                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
-                  </svg>
-                  <span className="sr-only">Loading...</span>
-                </div>
-              } Submit
+                <div>{
+                  loading && <div role="status">
+                    <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                      <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                    </svg>
+                    <span className="sr-only">Loading...</span>
+                  </div>
+                }  </div><div>Submit</div>
+
+
               </button>
             </div>
 
